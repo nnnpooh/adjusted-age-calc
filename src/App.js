f877053517abd791c5e5492ccb15b15594c999aa
Nirand <56184504+nnnpooh@users.noreply.github.com>
2021-06-03 01:45:45 +0700

    Finish first version

--- src/App.js
+++ src/App.js
@@ -1,16 +1,26 @@
 //import './App.css';
 import 'semantic-ui-css/semantic.min.css';
 import React, { useState } from 'react';
-import { addDays, differenceInDays } from 'date-fns';
-import { Input, Button } from 'semantic-ui-react';
+import { addDays, differenceInDays, format } from 'date-fns';
+import {
+  Input,
+  List,
+  Icon,
+  Header,
+  Grid,
+  Container,
+  Segment,
+  Menu,
+} from 'semantic-ui-react';
 import SemanticDatepicker from 'react-semantic-ui-datepickers';
 import 'react-semantic-ui-datepickers/dist/react-semantic-ui-datepickers.css';
 
 function App() {
-  const [birthDate, setBirthDate] = useState(new Date('02/14/2021'));
-  const [GAWeek, setGAWeek] = useState(25);
-  const [GADayAdd, setGADayAdd] = useState(0);
-  const [logDate, setLogDate] = useState(new Date('05/14/2021'));
+  const [birthDate, setBirthDate] = useState('');
+  const [GAWeek, setGAWeek] = useState(null);
+  const [GADayAdd, setGADayAdd] = useState(null);
+  const [logDate, setLogDate] = useState('');
+
   function splitWeekDay(numDay) {
     let week = 0;
     let dayAdd = 0;
@@ -25,16 +35,19 @@ function App() {
     return [week, dayAdd];
   }
 
-  const AdBoD = addDays(birthDate, (40 - GAWeek) * 7 - GADayAdd);
-  const CADay = differenceInDays(logDate, birthDate);
-  const [CAWeek, CADayAdd] = splitWeekDay(CADay);
-  const PCA = GAWeek * 7 + GADayAdd + CADay;
+  let AdBoD = '';
   let AJ = '';
-  let AJWeek = '';
-  let AJDayAdd = '';
-  if (differenceInDays(AdBoD, birthDate) > 0) {
-    AJ = GAWeek * 7 + GADayAdd + CADay - 280;
-    [AJWeek, AJDayAdd] = splitWeekDay(AJ);
+  let CADay = '';
+  let PCA = '';
+  let GA = '';
+  if (GAWeek && GADayAdd) GA = GAWeek * 7 + GADayAdd;
+  if (GAWeek && GADayAdd && birthDate && logDate) {
+    AdBoD = addDays(birthDate, (40 - GAWeek) * 7 - GADayAdd);
+    CADay = differenceInDays(logDate, birthDate);
+    PCA = GAWeek * 7 + GADayAdd + CADay;
+    if (differenceInDays(AdBoD, birthDate) > 0) {
+      AJ = GAWeek * 7 + GADayAdd + CADay - 280;
+    }
   }
 
   const onChangeBirthDayCalendar = (event, data) => setBirthDate(data.value);
@@ -44,7 +57,7 @@ function App() {
     if (value) {
       setGAWeek(value);
     } else {
-      setGAWeek(0);
+      setGAWeek(null);
     }
   };
 
@@ -53,28 +66,118 @@ function App() {
     if (value) {
       setGADayAdd(value);
     } else {
-      setGADayAdd(0);
+      setGADayAdd(null);
     }
   };
 
+  function displayListItem(text, icon, header) {
+    return (
+      <List.Item>
+        <Icon name={icon} />
+        <List.Content>
+          <List.Header>{header}</List.Header>
+          <List.Description>{text}</List.Description>
+        </List.Content>
+      </List.Item>
+    );
+  }
+
+  function formatTextDay(day) {
+    if (day) {
+      const [week, dayAdd] = splitWeekDay(day);
+      return `${day} วัน (${week} สัปดาห์ ${dayAdd} วัน)`;
+    } else {
+      return '';
+    }
+  }
+
+  function formatTextDate(d) {
+    if (Object.prototype.toString.call(d) === '[object Date]') {
+      // it is a date
+      if (isNaN(d.getTime())) {
+        // d.valueOf() could also work
+        // date is not valid
+        return '';
+      } else {
+        // date is valid
+        return format(d, 'EEEE dd/MM/yyyy');
+      }
+    } else {
+      // not a date
+      return '';
+    }
+  }
   return (
     <>
-      <h1>Hello</h1>
+      <Menu fixed='top' inverted color={'purple'} widths={1}>
+        <Container>
+          <Menu.Item>Adjusted Age Calculation</Menu.Item>
+        </Container>
+      </Menu>
+      <Container style={{ marginTop: '4em' }}>
+        <Segment>
+          <Grid stackable columns={3}>
+            <Grid.Column style={{ margin: '0em' }}>
+              <Header as='h4'>Date of Birth</Header>
+              <SemanticDatepicker
+                onChange={onChangeBirthDayCalendar}
+                format={'DD/MM/YYYY'}
+              />
+            </Grid.Column>
+            <Grid.Column style={{ margin: '0em' }}>
+              <Header as='h4'>Gestational Age, GA </Header>
+              <Input
+                label={{ basic: true, content: 'Week' }}
+                labelPosition='right'
+                placeholder='Week'
+                onChange={onChangeGAWeek}
+              />
+              <Input
+                label={{ basic: true, content: 'Day' }}
+                labelPosition='right'
+                placeholder='Day'
+                onChange={onChangeGADayAdd}
+                style={{ marginTop: '8px' }}
+              />
+            </Grid.Column>
+            <Grid.Column style={{ margin: '0em' }}>
+              <Header as='h4'>Today</Header>
+              <SemanticDatepicker
+                onChange={onChangeLogDateCalendar}
+                format={'DD/MM/YYYY'}
+              />
+            </Grid.Column>
+          </Grid>
+        </Segment>
+        <Segment>
+          <List relaxed>
+            {displayListItem(formatTextDay(GA), 'clock', 'Gestational Age, GA')}
 
-      <SemanticDatepicker onChange={onChangeBirthDayCalendar} />
-      <Input placeholder='อายุครรภ์ (สัปดาห์) ' onChange={onChangeGAWeek} />
-      <Input placeholder='อายุครรภ์ (วัน) ' onChange={onChangeGADayAdd} />
-      <SemanticDatepicker onChange={onChangeLogDateCalendar} />
+            {displayListItem(
+              formatTextDate(birthDate),
+              'birthday cake',
+              'Date of Birth'
+            )}
 
-      <p>{birthDate.toDateString()}</p>
-      <p>{AdBoD.toDateString()}</p>
-      <p>{CADay}</p>
-      <p>{CAWeek}</p>
-      <p>{CADayAdd}</p>
-      <p>{PCA}</p>
-      <p>{AJ}</p>
-      <p>{AJWeek}</p>
-      <p>{AJDayAdd}</p>
+            {displayListItem(
+              formatTextDate(AdBoD),
+              'birthday cake',
+              'Adjusted Date of Birth'
+            )}
+            {displayListItem(
+              formatTextDay(CADay),
+              'clock',
+              'Chronological Age, CA '
+            )}
+            {displayListItem(
+              formatTextDay(PCA),
+              'clock',
+              'Post Conceptional Age, PCA '
+            )}
+            {displayListItem(formatTextDay(AJ), 'time', 'Adjusted Age, AJ')}
+          </List>
+        </Segment>
+      </Container>
     </>
   );
 }
